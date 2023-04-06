633f2ae926902bab0b4b7aa59e1208098af046a6
Floflorentin <53.michel.florentin@gmail.com>
2023-04-06 13:10:52 +0200

    FM - Leetcode validparentheses

---
+++ src/main/java/fr/leetcode/easy/longest_palindrome/Solution.java
@@ -0,0 +1,30 @@
+package fr.leetcode.easy.longest_palindrome;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/longest-palindrome/
+ */
+public class Solution {
+    public int longestPalindrome(String s) {
+        int[] letters = new int[58]; // [A-z + ([|\)
+        Arrays.fill(letters, 0);
+
+        int sum = 0;
+        for(Character c: s.toCharArray()) {
+            int index = c - 65;
+            if(letters[index] == 1) {
+                sum+= 2;
+                letters[index] = 0;
+            } else {
+                letters[index] = 1;
+            }
+        }
+        for(int letter: letters) {
+            if(letter == 1) {
+                return sum + 1;
+            }
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/fr/leetcode/easy/longest_palindrome/SolutionTest.java
@@ -0,0 +1,24 @@
+package fr.leetcode.easy.longest_palindrome;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            aaaaaaa,7
+            a,1
+            AAAAAA,6
+            zeusnilemacaronimaisanitratetartinasiaminoracamelinsuez,55
+            """
+    )
+    void testSimple(String input, int output) {
+        var solution = new Solution();
+        assertEquals(output, solution.longestPalindrome(input));
+    }
+}
